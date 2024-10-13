b02298d6c3e0b1f8c8047c69b0265fad8790be13
Cláudio Junior <cpratesjunior@bol.com.br>
2024-10-13 11:01:45 -0300

    Exercicios de Javascript

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70.8
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if(vel > 60) { // condição simples
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija usando o cinto de segurança!')
